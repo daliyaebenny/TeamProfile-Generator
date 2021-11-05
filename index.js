df7fcf193cb815fecb19948d8befe39ca3793681
Daliya Benny <daliyaebenny@gmail.com>
2021-11-05 16:38:10 -0700

    Added inquirer

--- index.js
+++ index.js
@@ -1,59 +1,11 @@
 // Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Manager = require('./lib/Manager');
-//const employeeArray;
+
+const fetchEmployeeData = require("./src/addEmployee");
 
 
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log('Successfully created Team Profile ')
- );
+function start(){
+  let employees=[];
+  employees=fetchEmployeeData();//fetchEmployeeData() returns ana array of objects
+  console.log(employees[0]);
 }
-//function to fetch data using inquirer
-function fetchEmployeeType() {
-  inquirer
-  .prompt([
-    
-    {
-      type: 'list',
-      message: 'Choose the employee to be added',
-      name: 'employeeType',
-      choices: ['Manager', 'Intern', 'Engineer', 'Generate_Profile'],
-    }
-  ])
-  .then((data) => {
-    console.log(data);
-    
-    switch(data.employeeType) {
-        case 'Manager':
-        //   const manager = new Manager();
-            console.log("Manager");
-            fetchEmployeeType();
-          break;
-        case 'Intern':
-            console.log("Intern");
-            fetchEmployeeType();
-          break;
-          case 'Engineer':
-            console.log("Engineer");
-            fetchEmployeeType();
-          break;
-          case 'Generate_Profile':
-            writeToFile('./dist/fileName.html','abc');
-          break;
-        
-        default:
-          
-      }
-
-  });
-}
-//function to initialize app
-function init() {
-    fetchEmployeeType();
-}
-
-// Function call to initialize app
-init();
+start();
--- src/addEmployee.js
+++ src/addEmployee.js
@@ -1,9 +1,68 @@
-const Employee = require("../lib/Employee");
-const Manager = require("./lib/Manager");
+//const Manager = require("../lib/Manager");
+const inquirer = require('inquirer');
 
-function addManager(){
-  const employee = new Employee(name,id,email);
-  const manager = new Manager();
+  async function fetchEmployeeData(){
+    let employees=[];
+    let repeat =true;
+  do{
+   await inquirer.prompt([
 
+    {
+      type: 'input',
+      name: 'role',
+      message: 'Employee Role ',
+    },
     
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Employee Name ',
+    },
+    
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Employee Id',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Employee email',
+      
+    },
+    {
+      type: 'number',
+      name: 'officeNumber',
+      message: 'Employee Office number',
+      when: (data) => data.role === 'Manager',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Employee Github username',
+      when: (data) => data.role === 'Engineer',
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Employee School',
+      when: (data) => data.role === 'Intern',
+    },
+    {
+      type: "confirm",
+      name: "repeat",
+      message: "Do you want to add another employee ?",
+    },
+  
+  ])
+  .then((data) => {
+    repeat = data.repeat;
+    employees.push(data);
+  
+  });
+}while(repeat);
+console.log(employees);
+return employees;
 }
+
+module.exports=fetchEmployeeData;
